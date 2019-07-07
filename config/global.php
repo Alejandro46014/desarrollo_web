6c19c5d9f35ee56e596c04dff20486d87207f553
belight <alejandroalcaraz@fp.uoc.edu>
2019-07-07 21:32:43 +0200

    global.php

---
+++ config/global.php
@@ -0,0 +1,16 @@
+<?php
+//host
+
+define("db_host","localhost");
+
+//nombre base datos
+
+define("db_name","dbsistema");
+
+//usuario
+
+define("db_usser","Alejandro");
+
+//password
+
+define("db_password","3424");
